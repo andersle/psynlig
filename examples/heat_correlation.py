47064a0a9324b33a73e3eac9799906ee81e968f1
Anders Lervik <andersle@gmail.com>
2020-01-18 10:12:52 +0100

    Updated readme.

--- examples/heat_correlation.py
+++ examples/heat_correlation.py
@@ -12,24 +12,16 @@ def load_iris_set():
     """Set up the data set."""
     data_set = load_iris()
     data = pd.DataFrame(data_set['data'], columns=data_set['feature_names'])
-    data['target'] = data_set['target']
-    xvars = [
-        'sepal length (cm)',
-        'sepal width (cm)',
-        'petal length (cm)',
-        'petal width (cm)'
-    ]
-    xdata = data[xvars]
-    return data, xdata
+    return data
 
 
 def main():
     """Generate the heat map."""
-    _, xdata = load_iris_set()
-    plot_heatmap(xdata, vmin=-1, vmax=1)
-    plot_heatmap(xdata, textcolors=['white', 'black'], vmin=-1, vmax=1)
-    plot_heatmap(xdata, textcolors=['white', 'black'])
-    plot_heatmap(xdata, textcolors=['white'], cmap='Spectral')
+    data = load_iris_set()
+    plot_heatmap(data, vmin=-1, vmax=1)
+    plot_heatmap(data, textcolors=['white', 'black'], vmin=-1, vmax=1)
+    plot_heatmap(data, textcolors=['white', 'black'])
+    plot_heatmap(data, textcolors=['white'], cmap='Spectral')
     plt.show()
 
 
--- examples/heat_correlation_readme.py
+++ examples/heat_correlation_readme.py
@@ -1,12 +1,6 @@
-# psynlig
-
-A library for creating plots with matplotlib.
-
-## Examples
-
-### Generating a heat map for correlations
-The following code
-```python
+# Copyright (c) 2020, Anders Lervik.
+# Distributed under the MIT License. See LICENSE for more info.
+"""An example of generating a heat map of correlations."""
 from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.datasets import load_iris
@@ -19,9 +13,3 @@ data = pd.DataFrame(data_set['data'], columns=data_set['feature_names'])
 fig, _ = plot_heatmap(data, vmin=-1, vmax=1, textcolors=['white', 'black'])
 fig.savefig('heatmap_example.png', bbox_inches='tight')
 plt.show()
-```
-
-will generate the following figure:
-
-![heatmap](examples/heatmap_example.png)
-
