9a0bd3d5f67a4b571fddb1db27295a3916147f83
Anders Lervik <andersle@gmail.com>
2020-01-28 14:39:27 +0100

    Added outlier example.

--- docs/gallery/exploration/plot_08_scatter_1d_outlier.py
+++ docs/gallery/exploration/plot_08_scatter_1d_outlier.py
@@ -1,16 +1,12 @@
 # Copyright (c) 2020, Anders Lervik.
 # Distributed under the MIT License. See LICENSE for more info.
 r"""
-Generating 1D scatter plots
-===========================
+Generating 1D scatter plots with outliers
+=========================================
 
 This examle will plot the raw data points in a 1D scatter plot.
-
-The data points can be colored
-according to class labels if this is available.
-This is done by passing the labels for each data point (using
-the parameter ``class_data``) and a mapping from the labels
-to something more human-readable (using the parameter ``class_names``).
+Here, we attempt to highlight outliers by calculating the
+interquartile range.
 """
 from matplotlib import pyplot as plt
 import pandas as pd
